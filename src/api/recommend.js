62be5c4d76c7b80ed746fb4d39ead351120bbcdd
flyFatSea <491927227@qq.com>
2018-06-07 15:47:44 +0800

    delet part-4

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -15,31 +15,22 @@ export function getRecommend() {
 }
 
 export function getDiscList() {
-  const url = '/api/getDiscList'
-
-  const data = {
-    callback: 'recom07589760060363315',
-    g_tk: 5381,
-    jsonpCallback: 'recom07589760060363315',
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  const data = Object.assign({}, commonParams, {
+    picmid: 1,
     loginUin: 0,
     hostUin: 0,
     inCharset: 'utf8',
     outCharset: 'utf-8',
-    notice: 0,
     platform: 'yqq',
     sin: 0,
     ein: 29,
     sortId: 5,
     needNewCode: 0,
     categoryId: 10000000,
-    rnd: Math.random(),
-    format: 'json'
-  }
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data)
+    rnd: Math.random()
   })
+  return jsonp(url, data, options)
 }
 
 export function getSongList(disstid) {
